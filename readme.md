90aaa11be25dbd74e9c210472983fd89051b6a0c
AgentEnder <craigorycoppola@gmail.com>
2021-12-01 16:50:47 -0600

    chore(repo): setup solution runner
    Signed-off-by: AgentEnder <craigorycoppola@gmail.com>

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Polyglot AoC 2021
+
+This is my repo for Advent of Code 2021 problems. The repo is setup for solving the problems in C#, Python, Rust, and Typescript currently.
+
+## Running Solutions
+
+1. Run `npm i` 
+2. Run `npm start`
+
+This assumes the following commands are available on PATH
+- `python3 --version`
+- `dotnet --version`
+- `rustc --version`
+- `node`
+- `npx`
---
+++ run-solutions.ts
@@ -0,0 +1,109 @@
+import { execSync, ExecSyncOptionsWithBufferEncoding } from "child_process";
+import { red, yellow } from "colors";
+import { sync as globSync } from "fast-glob";
+import { readFileSync, writeFileSync } from "fs";
+import { extname, join, dirname, parse, resolve } from "path";
+import { performance } from "perf_hooks";
+import Ignore from "ignore";
+
+const problemDirectories = globSync("day-*", { onlyDirectories: true });
+
+const executeCommand: Record<
+    string,
+    (file) => string | { build: string; run: string }
+> = {
+    ".py": (file) => `python3 ${file}`,
+    ".csproj": (file) => {
+        const outDir = join(dirname(file), "build");
+        const dllPath = join(outDir, parse(file).name) + ".dll";
+        return {
+            build: `dotnet build ${file} -c Release -o ${outDir}`,
+            run: `dotnet ${dllPath}`,
+        };
+    },
+    ".rs": (file) => {
+        const name = parse(file).name;
+        return {
+            build: `rustc ${file} --out-dir build/${name}`,
+            run: `./build/${name}/${name}`,
+        };
+    },
+    ".ts": (file) => {
+        const name = parse(file).name;
+        return {
+            build: `npx tsc ${file} --outDir build`,
+            run: `node ./build/${name}.js`,
+        };
+    }
+};
+
+const ignore = Ignore();
+ignore.add(readFileSync(".gitignore").toString());
+
+problemDirectories.forEach((directory) => {
+    console.log("Evaluating solutions for", directory);
+    const solutionFiles = globSync(
+        `**/*@(${Object.keys(executeCommand).join("|")})`,
+        { cwd: directory }
+    ).filter(ignore.createFilter());
+    const lines: string[] = [];
+    const input = readFileSync(join(directory, "input.txt"));
+
+    for (const file of solutionFiles) {
+        const extension = extname(file);
+        const cwd = join(directory, dirname(file));
+        if (!(extension in executeCommand)) {
+            continue;
+        }
+        const relativePath = join(directory, file);
+        console.log("Running ", relativePath);
+        const command = executeCommand[extension](resolve(relativePath));
+        if (typeof command === "string") {
+            const start = performance.now();
+            const stdout = execSync(command, {
+                cwd,
+                input,
+            }).toString();
+            const end = performance.now();
+            lines.push(`${file}\t\t${formatTime(end - start)}`);
+            lines.push("");
+            lines.push(stdout);
+        } else {
+            const start = performance.now();
+            execSyncButLogErrors(command.build, {
+                cwd,
+            }).toString();
+            const buildEnd = performance.now();
+            const stdout = execSync(command.run, {
+                cwd,
+                input,
+            }).toString();
+            const end = performance.now();
+            lines.push(`${file}\t\t${formatTime(end - start)}`);
+            lines.push(
+                `Build ${formatTime(buildEnd - start)}, Run: ${formatTime(
+                    end - buildEnd
+                )}`
+            );
+            lines.push("");
+            lines.push(stdout);
+        }
+    }
+
+    writeFileSync(join(directory, "output.txt"), lines.join("\n"));
+});
+
+function execSyncButLogErrors(cmd, options: ExecSyncOptionsWithBufferEncoding) {
+    try {
+        return execSync(cmd, options);
+    } catch (e) {
+        console.log("\n", red("ERROR:"), e.message);
+        console.log("\n\t", yellow("stdout"), e.stdout.toString());
+        console.log("\n\t", yellow("stderr"), e.stderr.toString());
+        process.exit(1);
+    }
+}
+
+function formatTime(ms: number) {
+    return ms.toFixed(3);
+}
